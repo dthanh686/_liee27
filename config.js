5f7b9b3fae3c09e502a6ea2cbab290a4b7c0eddb
trducthanh <97324249+dthanh686@users.noreply.github.com>
2023-12-21 23:53:09 +0700

    Add files via upload
    add

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Click vào đây ikkkkk';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Anh biết nó khá là nhanh khi anh tìm hiểu em, nhưng anh đã để ý em từ trước lúc ở trung tâm rồi. Anh thực sự nghiêm túc muốn tìm hiểu em Cô giáo Ly ạ :3.Em cho anh cơ hội được đi chơi với em nhé. Run quá anh khum biết viết gì nữa @@';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
